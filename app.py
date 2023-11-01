0863252e3f1c9f5f01662509f8f3e9d0af8fa9f5
jeetshah5 <jeet5sanjay@gmail.com>
2023-11-01 08:39:27 +0530

    CSS

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
-        return render_template('home.html')
+        return render_template('home/home.html')
     else:
         data=CustomData(
             gender=request.form.get('gender'),
@@ -32,7 +32,7 @@ def predict_datapoint():
 
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
+        return render_template('home/home.html',results=results[0])
     
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True)
